cadf5004779291ee9e0311929c4d91ff27d4acfa
seong su kim <know.warehouse02@gmail.com>
2025-06-28 00:22:33 +0900

    테스트용 브랜치 / 디자인 갈아엎는 버전

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="app">
+        <div className="app app-bg">
           <Header />
           <main>
             <Routes>
